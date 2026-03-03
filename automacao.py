5a21c36e33596cfd222ae43e7af31a76e7e4bfc0
digu-dev <rodrigo.dev.contact@gmail.com>
2026-03-02 21:49:25 -0300

    mudanças

--- automacao.py
+++ automacao.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 import pyautogui
 
-pyautogui.PAUSE = 0.5  # Adiciona uma pausa de meio segundo entre as ações
+pyautogui.PAUSE = 0.8  # Adiciona uma pausa de meio segundo entre as ações
 link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 
 #pyautogui.click -> clica
@@ -45,50 +45,52 @@ pyautogui.press('tab')  # Pressiona a tecla Tab para ir para o botão de logar
 pyautogui.press('enter')  # Pressiona a tecla Enter para clicar no botão
 time.sleep(5)  # Aguarda 2 segundos para a página carregar
 
-pyautogui.click(x=1519, y=506)  # Clique para fora dos campos de email e senha
+pyautogui.click(x=1166, y=460)  # Clique para fechar popup
 time.sleep(2)  # Aguarda 2 segundos para a página carregar
 
 #3 abrir a base de dados
-table = pd.read_csv('produtos.csv')
-print(table)
+tabela = pd.read_csv('produtos.csv')
+print(tabela)
 
 #4 cadastrar 1 produto
 
-for line in table.index:
-    pyautogui.click(x=827, y=367)  # Clique para o campo de nome do produto
+for linha in tabela.index:
     
+   
     #codigo
-    codigo = str(table.loc[line, "codigo"])
+    
+    pyautogui.click(x=808, y=362)  # Clique para o campo de nome do produto   
+    codigo = str(tabela.loc[linha, "codigo"])
     pyautogui.write(codigo)  # Digite o codigo do produto
     pyautogui.press('tab')  # Pressiona a tecla Tab para ir para o campo de preço
     
     #marca
-    marca = str(table.loc[line, "marca"])
+    marca = str(tabela.loc[linha, "marca"])
     pyautogui.write(marca)  # Digite a marca do produto
     pyautogui.press('tab')  # Pressiona a tecla Tab para ir para o campo de quantidade
 
     #tipo
-    tipo = str(table.loc[line, "tipo"])
-    pyautogui.write(str(table.loc[line, "tipo"]))  # Digite o tipo do produto
+    tipo = str(tabela.loc[linha, "tipo"])
+    pyautogui.write(tipo)  # Digite o tipo do produto
     pyautogui.press('tab')  # Pressiona a tecla Enter para cadastrar o produto
 
     #categoria
-    categoria = str(table.loc[line, "categoria"])
-    pyautogui.write(str(table.loc[line, "categoria"]))  # Digite a categoria do produto
+    categoria = str(tabela.loc[linha, "categoria"])
+    pyautogui.write(categoria)  # Digite a categoria do produto
     pyautogui.press('tab')  # Pressiona a tecla Tab para ir para o campo de preço
 
     #preco_unitario
-    preco_unitario = str(table.loc[line, "preco_unitario"])
+    preco_unitario = str(tabela.loc[linha, "preco_unitario"])
     pyautogui.write(preco_unitario)  # Digite o preço do produto
     pyautogui.press('tab')  # Pressiona a tecla Tab para ir para o campo de custo
 
     #custo
-    custo = str(table.loc[line, "custo"])
-    pyautogui.write(str(table.loc[line, "custo"]))  # Digite o custo do produto
+    custo = str(tabela.loc[linha, "custo"])
+    pyautogui.write(custo)  # Digite o custo do produto
     pyautogui.press('tab')  # Pressiona a tecla Tab para ir para o campo de observação
 
     #obs
-    obs = str(table.loc[line, "obs"])
+    obs = str(tabela.loc[linha, "obs"])
     pyautogui.write(obs)  # Digite a observação do produto 
     pyautogui.press('tab')  # Pressiona a tecla tab para ir para o campo de cadastrar
     pyautogui.press('enter')  # Pressiona a tecla Enter para cadastrar o produto
--- auxilio.py
+++ auxilio.py
@@ -1,10 +1,11 @@
 import time
 import pyautogui
 
-time.sleep(7)  # Aguarda 5 segundos para o usuário posicionar o mouse
+time.sleep(4)  # Aguarda 5 segundos para o usuário posicionar o mouse
 print(pyautogui.position())
 #print("Point(x=982, y=504)") #clique para o campo de email
 #print("Point(x=822, y=643)") #clique para o campo de senha
 #print("Point(x=963, y=712)") #clique para o campo de logar
 #print("Point(x=1519, y=506))") #clique para fora dos campos de email e senha
 #print("Point(x=827, y=367)") #clique para o campo de nome do produto
+#print("Point(x=1166, y=460))") #clique para fechar popup
